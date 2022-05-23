31c7ed136d8983134962e7d78c138022fed4e5b6
Christopher Shaw <chris@chris-shaw.com>
2022-05-23 11:34:02 +0100

    Add testProgram003

---
+++ testProgram003/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CPD
+{
+
+    class Program
+    {
+
+        static void Main (string[] args)
+        {
+        
+            string filename;
+            int importType;
+            
+
+
+            Console.WriteLine("What File do you want to import?");
+            filename = Console.ReadLine();
+
+            Console.WriteLine("[1] Standard Import, [2] Advanced Import");
+            importType = Convert.ToInt16(Console.ReadLine());
+
+
+            switch(importType)
+            {
+                case 1:
+                    Console.WriteLine("Import File '" + filename + "' as a Standard Import");
+                    break;
+                case 2: 
+                    Console.WriteLine("Import File '" + filename + "' as a Advanced Import");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Import Type Selection");
+                    break;
+            }
+
+
+
+            Console.WriteLine("Import Completed");
+            Console.ReadKey();
+            
+
+            return;
+
+        }
+
+     
+
+    }
+
+
+}
---
+++ testProgram003/README.md
@@ -0,0 +1,1 @@
+A Mock Importer, just to use Console Input with a Switch Statement
