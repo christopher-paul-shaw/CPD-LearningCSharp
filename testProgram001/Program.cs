a378ef571af44aaefe6e90acb8691ca3ec91a490
Christopher Shaw <chris@chris-shaw.com>
2022-05-23 10:30:23 +0100

    Init Commit

---
+++ testProgram001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace CPD
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {    
+            setup();
+            interactiveText();
+            Console.ReadKey();
+        }
+
+        static void interactiveText() {         
+            string name;
+            string answer1; 
+            Console.WriteLine("Hello, What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", I am a Cheezy Scripted Program");
+            Console.WriteLine("What is your preferred Colour?");
+            answer1 = Console.ReadLine();
+            Console.WriteLine("So you like " + answer1 + " mine is Green");
+            return;
+        }
+
+        static void setup () {
+            Console.Title = "CPD Program";
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+    }
+}
+
+
+
